18c9f7e50d097b6d75e24186127ea731edd16bd1
SONY-VAIO <dmitry.luschik@yandex.by>
2022-04-15 18:31:36 +0300

    Improved the method by using the Integer wrapper

--- src/by/kursy/luschik/javastages/stage12/taskA1/modellogic/ExtremeValues.java
+++ src/by/kursy/luschik/javastages/stage12/taskA1/modellogic/ExtremeValues.java
@@ -1,31 +1,36 @@
 package by.kursy.luschik.javastages.stage12.taskA1.modellogic;
 
 public class ExtremeValues {
-    public static double findMin(double min, double...arrays){
-        for (double element:arrays
-             ) {
-            if (element<min){
-                min=element;
+    public static double findMin(double... arrays) {
+        double min = Integer.MAX_VALUE;
+        for (double element : arrays
+        ) {
+            if (element < min) {
+                min = element;
             }
         }
         return min;
     }
 
-    public static double findMax(double max, double...arrays){
-        for (double element:arrays
+    public static double findMax(double... arrays) {
+        double max = Integer.MIN_VALUE;
+        for (double element : arrays
         ) {
-            if (element>max){
-                max=element;
+            if (element > max) {
+                max = element;
             }
         }
         return max;
     }
-    public static double findAverage (double...arrays){
-        int count=0;
-        double sum=0;
-        for (double element :arrays
-             ) {sum+=element;
-        count++;}
-        return sum/count;
+
+    public static double findAverage(double... arrays) {
+        int count = 0;
+        double sum = 0;
+        for (double element : arrays
+        ) {
+            sum += element;
+            count++;
+        }
+        return sum / count;
     }
 }
