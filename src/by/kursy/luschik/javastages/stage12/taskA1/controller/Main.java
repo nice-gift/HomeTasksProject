280a13ba3ee147302db4343be19ea4b45ad6a2f5
SONY-VAIO <dmitry.luschik@yandex.by>
2022-04-15 18:35:02 +0300

    realized the output of all methods

--- src/by/kursy/luschik/javastages/stage12/taskA1/controller/Main.java
+++ src/by/kursy/luschik/javastages/stage12/taskA1/controller/Main.java
@@ -1,6 +1,6 @@
 package by.kursy.luschik.javastages.stage12.taskA1.controller;
 
-import by.kursy.luschik.javastages.stage12.util.ArrayInitializer;
+import by.kursy.luschik.javastages.stage12.taskA1.modellogic.ExtremeValues;
 import by.kursy.luschik.javastages.stage12.util.ArrayInitializerVarArgs;
 import by.kursy.luschik.javastages.stage12.util.Convertor;
 import by.kursy.luschik.javastages.view.Printer;
@@ -9,20 +9,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
-        int size=0;
+        Scanner scanner = new Scanner(System.in);
+        int size;
         do {
             Printer.print("Input size of array: ");
-            size= scanner.nextInt();
-        } while (size<0);
-        double[]array=new double[size];
-//        ArrayInitializer.consoleInit(array);
-//Printer.print("\n Array: "+ Convertor.convert(array));
+            size = scanner.nextInt();
+        } while (size < 1);
+        double[] array = new double[size];
 
-//ArrayInitializer.rndInit(array);
-//        Printer.print("\nArray: "+ Convertor.convert(array));
+//        ArrayInitializer.consoleInit(array);
+//        Printer.print("\n Array: " + Convertor.convert(array));
+//
+//        ArrayInitializer.rndInit(array);
+//        Printer.print("\nArray: " + Convertor.convert(array));
 
         ArrayInitializerVarArgs.rndInit(array);
-        Printer.print("\nArray: "+ Convertor.convert(array));
+        Printer.print("\nArray: " + Convertor.convert(array));
+
+        String min_value = String.format("\nMin value is %.2f",
+                ExtremeValues.findMin(array));
+        Printer.print(min_value);
+
+        String max_value = String.format("\nMax value is %.2f",
+                ExtremeValues.findMax(array));
+        Printer.print(max_value);
+
+        String average = String.format("\nAverage is %.2f",
+                ExtremeValues.findAverage(array));
+        Printer.print(average);
     }
 }
