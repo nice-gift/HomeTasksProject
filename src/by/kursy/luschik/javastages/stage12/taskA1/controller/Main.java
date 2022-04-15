3a5491be862b05853fde5858ba5b5097dec38aca
nice-gift <dmitry.luschick@yandex.by>
2022-04-15 15:06:56 +0300

    Change Main class for simple cases

--- src/by/kursy/luschik/javastages/stage12/taskA1/controller/Main.java
+++ src/by/kursy/luschik/javastages/stage12/taskA1/controller/Main.java
@@ -1,4 +1,28 @@
 package by.kursy.luschik.javastages.stage12.taskA1.controller;
 
+import by.kursy.luschik.javastages.stage12.util.ArrayInitializer;
+import by.kursy.luschik.javastages.stage12.util.ArrayInitializerVarArgs;
+import by.kursy.luschik.javastages.stage12.util.Convertor;
+import by.kursy.luschik.javastages.view.Printer;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        int size=0;
+        do {
+            Printer.print("Input size of array: ");
+            size= scanner.nextInt();
+        } while (size<0);
+        double[]array=new double[size];
+//        ArrayInitializer.consoleInit(array);
+//Printer.print("\n Array: "+ Convertor.convert(array));
+
+//ArrayInitializer.rndInit(array);
+//        Printer.print("\nArray: "+ Convertor.convert(array));
+
+        ArrayInitializerVarArgs.rndInit(array);
+        Printer.print("\nArray: "+ Convertor.convert(array));
+    }
 }
