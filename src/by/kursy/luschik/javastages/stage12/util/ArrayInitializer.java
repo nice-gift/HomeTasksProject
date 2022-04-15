bfaec51ab1f2faa288cd0ab8f6342dd8dca058be
SONY-VAIO <dmitry.luschik@yandex.by>
2022-04-15 18:33:10 +0300

    just auto-formatting

--- src/by/kursy/luschik/javastages/stage12/util/ArrayInitializer.java
+++ src/by/kursy/luschik/javastages/stage12/util/ArrayInitializer.java
@@ -6,23 +6,23 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class ArrayInitializer {
-    public static void rndInit (double [] array){
-        Scanner scanner= new Scanner(System.in);
-        Random random= new Random();
+    public static void rndInit(double[] array) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
         Printer.print("Input min value of random: ");
-        double min= scanner.nextDouble();
+        double min = scanner.nextDouble();
         Printer.print("Input max value of random: ");
-        double max= scanner.nextDouble();
+        double max = scanner.nextDouble();
         for (int i = 0; i < array.length; i++) {
-            array [i]=(min+(max-min)*random.nextDouble());
+            array[i] = (min + (max - min) * random.nextDouble());
         }
     }
 
-    public static void consoleInit (double[]array){
-        Scanner scanner= new Scanner(System.in);
+    public static void consoleInit(double[] array) {
+        Scanner scanner = new Scanner(System.in);
         Printer.print("Input array elements: ");
         for (int i = 0; i < array.length; i++) {
-            array[i]= scanner.nextDouble();
+            array[i] = scanner.nextDouble();
         }
     }
 }
--- src/by/kursy/luschik/javastages/stage12/util/Convertor.java
+++ src/by/kursy/luschik/javastages/stage12/util/Convertor.java
@@ -2,10 +2,10 @@ package by.kursy.luschik.javastages.stage12.util;
 
 
 public class Convertor {
-    public static String convert(double[] array) {
+    public static String convert(double... arrays) {
         StringBuilder builder = new StringBuilder();
-        for (double element : array) {
-            String result=String.format("%.2f",element);
+        for (double element : arrays) {
+            String result = String.format("%.2f", element);
             builder.append(result).append(" ");
         }
         return builder + "";
