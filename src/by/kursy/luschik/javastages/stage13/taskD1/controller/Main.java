aa8fcd507c5add6b359f3317fd564021fb48e21a
SONY-VAIO <dmitry.luschik@yandex.by>
2022-04-25 20:08:19 +0300

    controller is ready

--- src/by/kursy/luschik/javastages/stage13/taskD1/controller/Main.java
+++ src/by/kursy/luschik/javastages/stage13/taskD1/controller/Main.java
@@ -1,4 +1,26 @@
 package by.kursy.luschik.javastages.stage13.taskD1.controller;
 
+import by.kursy.luschik.javastages.stage13.taskD1.modellogic.Verify;
+import by.kursy.luschik.javastages.stage13.util.ArrayInitInt;
+import by.kursy.luschik.javastages.view.Printer;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size;
+        do {
+            Printer.print("Enter a number of element: ");
+            size = scanner.nextInt();
+        } while (size < 1);
+        int[] array = new int[size];
+        ArrayInitInt.consoleInit(size, array);
+        Printer.print("Your numbers: " + Arrays.toString(array));
+
+        String msg = Verify.verifyAllNumsAreDif(array) ? "All numbers " +
+                "are different" : "There are repeated numbers";
+        Printer.print("\n"+msg);
+    }
 }
