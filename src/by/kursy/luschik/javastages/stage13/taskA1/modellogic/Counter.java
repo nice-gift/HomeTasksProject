c13acf5c513e17566b7d087d7dc743a9ee3cd139
nice-gift <dmitry.luschick@yandex.by>
2022-04-25 11:42:33 +0300

    created a simple method for constructing logic

---
+++ src/by/kursy/luschik/javastages/stage13/taskA1/modellogic/Counter.java
@@ -0,0 +1,9 @@
+package by.kursy.luschik.javastages.stage13.taskA1.modellogic;
+
+public class Counter {
+    public static int countNotNull(int... arrays) {
+        int summ=0;
+
+        return summ;
+    }
+}
