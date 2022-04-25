e1bc14ae1053ca447da2f082b067c2a64c29f8ac
nice-gift <dmitry.luschick@yandex.by>
2022-04-25 16:28:23 +0300

    new method

---
+++ src/by/kursy/luschik/javastages/stage13/taskС1/modellogic/Counter.java
@@ -0,0 +1,9 @@
+package by.kursy.luschik.javastages.stage13.taskС1.modellogic;
+
+public class Counter {
+    public static int countNumsBiggerAvg(double avg, int... arrays) {
+        int count = 0;
+
+        return count;
+    }
+}
