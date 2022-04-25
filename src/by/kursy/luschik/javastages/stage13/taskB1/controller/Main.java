80dbe97fd4a437ba4478cc402678c9d31a6563c7
nice-gift <dmitry.luschick@yandex.by>
2022-04-25 13:14:02 +0300

    put the element input first and then the array output

--- src/by/kursy/luschik/javastages/stage13/taskB1/controller/Main.java
+++ src/by/kursy/luschik/javastages/stage13/taskB1/controller/Main.java
@@ -1,6 +1,6 @@
 package by.kursy.luschik.javastages.stage13.taskB1.controller;
 
-import by.kursy.luschik.javastages.stage13.taskA1.modellogic.Counter;
+import by.kursy.luschik.javastages.stage13.taskB1.modellogic.Counter;
 import by.kursy.luschik.javastages.stage13.util.ArrayInitInt;
 import by.kursy.luschik.javastages.view.Printer;
 
@@ -18,13 +18,13 @@ public class Main {
         int[] array = new int[size];
 
         ArrayInitInt.rndInit(array);
-        Printer.print("Your numbers: " + Arrays.toString(array));
-
         Printer.print("Enter the number to be compared with the " +
                 "elements of the array: ");
         int bound = scanner.nextInt();
+        Printer.print("Your numbers: " + Arrays.toString(array));
+
         String msg = String.format("\nCount of numbers more than %d: %d",
-                bound, Counter.countNotNull(bound, array));
+                bound, Counter.countNumsGreater(bound, array));
         Printer.print(msg);
     }
 }
