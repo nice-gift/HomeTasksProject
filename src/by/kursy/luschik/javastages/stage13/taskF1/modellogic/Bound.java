bf61e883fb3b1144e3024c073e222c927a455891
nice-gift <dmitry.luschick@yandex.by>
2022-04-26 16:17:24 +0300

    Add the method to get index of 0-element

---
+++ src/by/kursy/luschik/javastages/stage13/taskF1/modellogic/Bound.java
@@ -0,0 +1,26 @@
+package by.kursy.luschik.javastages.stage13.taskF1.modellogic;
+
+public class Bound {
+    public static int getIndexFirst0(double... numbers) {
+        int indexFirst0 = numbers.length;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == 0) {
+                indexFirst0 = i;
+                break;
+            }
+        }
+        return indexFirst0;
+    }
+
+    public static int getIndexLast0(double... numbers) {
+        int indexLast0 = 0;
+        for (int i = numbers.length; i > 0; i--) {
+            if (numbers[i] == 0) {
+                indexLast0 = i;
+                break;
+            }
+        }
+        return indexLast0;
+    }
+
+}
