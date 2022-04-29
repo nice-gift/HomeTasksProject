efd459aea594f4ef8a0381c78a26a377a1218307
SONY-VAIO <dmitry.luschik@yandex.by>
2022-04-29 23:26:09 +0300

    New method selectedSort

--- src/by/kursy/luschik/javastages/stage13/util/ArraySorter.java
+++ src/by/kursy/luschik/javastages/stage13/util/ArraySorter.java
@@ -1,8 +1,8 @@
 package by.kursy.luschik.javastages.stage13.util;
 
 public class ArraySorter {
-    public static void insertedSortDsc(int indexFirst0, double[] array) {
-        for (int i = 1; i < indexFirst0; i++) {
+    public static void insertedSortDsc(int indexBound, double[] array) {
+        for (int i = 1; i < indexBound; i++) {
             double element = array[i];
             int j = i - 1;
             while (j >= 0 && array[j] < element) {
@@ -10,6 +10,30 @@ public class ArraySorter {
                 j--;
             }
             array[j + 1] = element;
+        }
+    }
+
+    public static void selectedSortDsc(int indexBound, double[] array) {
+        for (int i = 0; i < indexBound - 1; i++) {
+            int indexMax = i;
+            boolean flag = true;
+            for (int k = 0; k < array[indexBound - 1]; k++) {
+                if (array[k] > array[k + 1]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                break;
+            }
+            for (int j = i + 1; j < indexBound; j++) {
+                if (array[indexMax] < array[j]) {
+                    indexMax = j;
+                }
+            }
+            double t = array[i];
+            array[i] = array[indexMax];
+            array[indexMax] = t;
         }
     }
 }
