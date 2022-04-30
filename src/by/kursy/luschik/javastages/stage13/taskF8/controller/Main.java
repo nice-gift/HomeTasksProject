87855e27f925d216e1b0364f2e88ef3cdb566391
SONY-VAIO <dmitry.luschik@yandex.by>
2022-04-30 10:29:36 +0300

    realized method

--- src/by/kursy/luschik/javastages/stage13/taskF8/controller/Main.java
+++ src/by/kursy/luschik/javastages/stage13/taskF8/controller/Main.java
@@ -1,4 +1,33 @@
 package by.kursy.luschik.javastages.stage13.taskF8.controller;
 
+import by.kursy.luschik.javastages.stage13.taskF8.modellogic.Bound;
+import by.kursy.luschik.javastages.stage13.util.ArrayInitInt;
+import by.kursy.luschik.javastages.stage13.util.ArraySorter;
+import by.kursy.luschik.javastages.stage13.util.UserInput;
+import by.kursy.luschik.javastages.view.Printer;
+
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int size = UserInput.input("Input a count of numbers: ");
+
+        int[] array = new int[size];
+
+        ArrayInitInt.consoleInit(array);
+
+        Printer.print("Your numbers: " + Arrays.toString(array));
+
+        int indexFirstOddNum = Bound.getIndexFirstOdd(array);
+        ArraySorter.selectedSortAsc(indexFirstOddNum, array);
+
+        Printer.print("\nNumbers after sorting by ascending until the first" +
+                " odd element: " + Arrays.toString(array));
+
+        int indexLastOddNum = Bound.getIndexLastOdd(array);
+        ArraySorter.selectedSortAsc(indexLastOddNum, array);
+
+        Printer.print("\nNumbers after sorting by ascending until the last" +
+                " odd element: " + Arrays.toString(array));
+    }
 }
--- src/by/kursy/luschik/javastages/stage13/taskF8/modellogic/Bound.java
+++ src/by/kursy/luschik/javastages/stage13/taskF8/modellogic/Bound.java
@@ -1,4 +1,29 @@
 package by.kursy.luschik.javastages.stage13.taskF8.modellogic;
 
 public class Bound {
+    public static int getIndexFirstOdd(int... numbers) {
+        int index = numbers.length;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 1) {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+
+    public static int getIndexLastOdd(int... numbers) {
+        int index = numbers.length;
+
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            if (numbers[i] % 2 == 1) {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
 }
