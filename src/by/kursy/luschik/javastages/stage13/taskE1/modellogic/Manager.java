abf9998d99ef03e0efed68f13acc8981468df9f5
SONY-VAIO <dmitry.luschik@yandex.by>
2022-04-30 13:27:12 +0300

    refactoring, decrease a count of iterations in loop

--- src/by/kursy/luschik/javastages/stage13/taskE1/modellogic/Manager.java
+++ src/by/kursy/luschik/javastages/stage13/taskE1/modellogic/Manager.java
@@ -2,10 +2,10 @@ package by.kursy.luschik.javastages.stage13.taskE1.modellogic;
 
 public class Manager {
     public static int getMax(double... numbers) {
-        double max = Double.MIN_VALUE;
         int indexMax = 0;
+        double max = numbers[indexMax];
 
-        for (int i = 0; i < numbers.length; i++) {
+        for (int i = 1; i < numbers.length; i++) {
             if (numbers[i] > max) {
                 max = numbers[i];
                 indexMax = i;
@@ -16,10 +16,10 @@ public class Manager {
     }
 
     public static int getMIn(double... numbers) {
-        double min = Double.MAX_VALUE;
         int indexMin = 0;
+        double min = numbers[indexMin];
 
-        for (int i = 0; i < numbers.length; i++) {
+        for (int i = 1; i < numbers.length; i++) {
             if (numbers[i] < min) {
                 min = numbers[i];
                 indexMin = i;
